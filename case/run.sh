a812fb1b8c1c48448add58c0bdcc8bcfafc1b6c1
suzm10 <suzanmanasreh@gmail.com>
2023-11-03 10:45:43 -0400

    gitignore and script

---
+++ case/run.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#Do the following before running
+#change initcond.F90 layers variable
+#change the #cpus and #cores here
+#change log file name to reflect proc and layer count
+#change num processes in srun to reflect the total execution threads
+
+#run this in the case directory of RBC3D
+
+#SBATCH --account=gts-sbryngelson3
+#SBATCH -N2 --ntasks-per-node=24
+#SBATCH -t4
+#SBATCH --mem-per-cpu=2G
+#SBATCH -t8:00:00
+#SBATCH -q embers
+#SBATCH --mail-user=smanasreh6@gatech.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -o "./run_logs/init_run2.log"
+
+cd $SLURM_SUBMIT_DIR
+
+cd D
+rm -rf x*
+rm -rf r*
+rm -rf w*
+cd ../
+
+ml gcc mvapich2 netcdf-c netcdf-cxx netcdf-fortran fftw
+
+cd ../common
+make clean
+make .depend
+make
+
+cd ../case
+make clean
+make .depend
+make
+srun -n 2 ./initcond
+srun -n 2 -p 24 ./tube
