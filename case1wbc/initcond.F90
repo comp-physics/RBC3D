eb8eaba03b0710c4c7bdf4c3042f91ee5bac630b
suzm10 <suzanmanasreh@gmail.com>
2024-01-17 22:48:57 -0500

    new case

---
+++ case1wbc/initcond.F90
@@ -0,0 +1,193 @@
+! Create restart file with initial condition
+program InitCond
+
+  use ModDataTypes
+  use ModDataStruct
+  use ModRbc
+  use ModWall
+  use ModConf
+  use ModData
+  use ModIO
+  use ModBasicMath
+  use MPI
+
+  implicit none
+
+  integer,parameter :: nrbcMax = 128
+  integer,parameter :: nwallMax = 4
+  type(t_rbc),pointer :: rbc
+  type(t_rbc)         :: rbcRef
+  type(t_wall),pointer :: wall
+  real(WP) :: radEqv, szCell(3)
+  integer :: nlat0, ii
+  real(WP) :: theta, th, rc, xc(3), ztemp
+  real(WP) :: xmin, xmax, ymin, ymax, zmin, zmax
+  integer :: iz, i, p, l, dealias
+  integer,parameter :: ranseed = 161269
+  character(CHRLEN) :: fn
+  ! real = double, fp
+  real :: lengtube,lengspacing, phi, actlen
+  real(WP) :: rand(27, 3)
+  integer :: j, ierr, half2, index, layer, newiz
+  real :: wbcs(2), tubeDiam, layerx(3), layery(3)
+
+    ! Initialize
+    call InitMPI
+    ! Allocate working arrays
+    allocate(rbcs(nrbcMax))
+    allocate(walls(nwallMax))
+
+    tubeDiam = 10.0
+
+    ! Wall
+    nwall = 1
+    wall=>walls(1)
+
+    ! if (10.ge.12) then
+    !     call ReadMyWallMesh('Input/cylinder_D_8.01_L_13.39.dat', wall)
+    !     actlen = 13.3921
+    ! else
+        call ReadWallMesh('Input/cylwithhole.e',wall)
+        actlen = 13.33
+    ! end if
+
+    nrbc = 1
+    nlat0 = 12
+    dealias = 3
+    phi = 70/real(100)
+    lengtube = 13.33 ! nrbc/real(phi) !XXLL
+
+    lengspacing = (lengtube - ((2.62 / 2.82) * 9)) / 9 ! lengtube/Real(nrbc)
+
+    print*, 'lengtube 1', lengtube
+    print*, 'lengspacing 1', lengspacing
+
+    wall%f = 0.
+
+    do i = 1,wall%nvert
+        th = ATAN2(wall%x(i,1),wall%x(i,2))
+        ! 10 is the diameter
+        wall%x(i,1) = tubeDiam/2.0*COS(th)    !!!!!!!!!!!!!!!!!!!!!!
+        wall%x(i,2) = tubeDiam/2.0*SIN(th)    !!!!!!!!!!!!!!!!!!!!!!
+        wall%x(i,3) = lengtube/actlen*wall%x(i,3)   !!!!!!!!!!!!!!!!!!!
+    end do
+    xmin = minval(wall%x(:,1))
+    xmax = maxval(wall%x(:,1))
+
+    ymin = minval(wall%x(:,2))
+    ymax = maxval(wall%x(:,2))
+
+    zmin = minval(wall%x(:,3))
+    zmax = maxval(wall%x(:,3))
+
+    ! size of the periodic box
+    Lb(1) = xmax - xmin + 0.5
+    Lb(2) = Lb(1)
+    Lb(3) = zmax - zmin
+    lengtube = Lb(3)
+    ! lengspacing = lengtube/real(nrbc)
+    print*, 'lengtube 2', lengtube
+    print*, 'lengspacing 2', lengspacing
+
+    ! Reference cell i
+    xc = 0.
+    ! radius of cell gets trans to ~1.4
+    ! if you want rad of 8, pass in 5.7 for 8/1.4 (5.7*1.4=8)
+    radEqv = 1.0
+
+    call Rbc_Create(rbcRef, nlat0, dealias)
+    call Rbc_MakeBiconcave(rbcRef, radEqv, xc)
+
+    do ii = 1,3
+        szCell(ii) = maxval(rbcRef%x(:,:,ii)) - minval(rbcRef%x(:,:,ii))
+    end do
+
+    xc(1) = 0
+    xc(2) = 0
+    xc(3) = 2
+    rbc => rbcs(1)
+    rbc%celltype = 2
+    call Rbc_Create(rbc, nlat0, dealias)
+    call Rbc_MakeLeukocyte(rbc, radEqv, xc)
+
+    ! Put things in the middle of the periodic box
+    call Recenter_Cells_and_Walls
+
+    ! Output
+    write(*, '(A,3F10.3)') 'Periodic domain size = ', Lb
+
+    Nt0 = 0; time = 0.
+    vBkg(1:2) = 0.; vBkg(3) = 8.
+
+    ! Write intial conditions
+    if (nrbc > 0) then
+        write(fn, FMT=fn_FMT) 'D/', 'x', 0, '.dat'
+        call WriteManyRBCs(fn, nrbc, rbcs )
+        write(*, '(A,A)') 'Cell file: ', trim(fn)
+
+        fn = 'D/restart.LATEST.dat'
+        call WriteRestart(fn, Nt0, time)
+        write(*, '(A,A)') 'Binary restart file: ', trim(fn)
+    end if
+
+    if (nwall > 0) then
+        write(fn, FMT=fn_FMT) 'D/', 'wall', 0, '.dat'
+        call WriteManyWalls(fn, nwall, walls )
+        write(*, '(A,A)') 'Wall file: ', trim(fn)
+    end if
+
+    write(fn, FMT=fn_FMT) 'D/', 'restart', Nt0, '.dat'
+    call WriteRestart(fn, Nt0, time)
+    write(*, '(A,A)') 'Binary restart file: ', trim(fn)
+
+    ! Deallocate working arrays
+    deallocate(rbcs)
+
+    ! Finalize
+    call FinalizeMPI
+
+    stop
+
+contains
+
+subroutine Recenter_Cells_and_Walls
+    integer :: irbc, iwall, ii, gen, repeat, j
+    real :: x, y, a
+    real, parameter :: PI = 3.14159265359
+    type(t_rbc),pointer :: rbc
+    type(t_wall),pointer :: wall
+    real(WP) :: xmin(3), xmax(3), xc(3)
+
+    ! cells
+    ! translate cells
+    do irbc = 1, nrbc
+        rbc => rbcs(irbc)
+        rbc%x(:,:,1) = rbc%x(:,:,1) + 0.5*Lb(1)
+        rbc%x(:,:,2) = rbc%x(:,:,2) + 0.5*Lb(2)
+        print*,'Zc', sum(rbc%x(:,:,3))/real(rbc%nlat*rbc%nlon)
+    end do
+
+    ! walls
+    ! find the bounding box
+    xmin = 1.D10
+    xmax = -1.D10
+    do iwall = 1,nwall
+        wall => walls(iwall)
+        do ii = 1,3
+            xmin(ii) = min(xmin(ii), minval(wall%x(:,ii)) )
+            xmax(ii) = max(xmax(ii), maxval(wall%x(:,ii)) )
+        end do
+    end do
+    xc = 0.5*(xmin + xmax)
+
+    ! translate walls
+    do iwall = 1,nwall
+        wall => walls(iwall)
+        do ii = 1,3
+            wall%x(:,ii) = wall%x(:,ii) + 0.5*Lb(ii) - xc(ii)
+        end do
+    end do
+
+end subroutine Recenter_Cells_and_Walls
+
+end program InitCond
