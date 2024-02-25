84dbec5df78f2052820357594d12e24a48dd78c8
suzm10 <suzanmanasreh@gmail.com>
2024-02-25 09:25:47 -0500

    script

--- format.sh
+++ format.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# on pace cluster, run these 2 commands to install fprettify
+# on pace cluster, run these commands to install fprettify
 # ml python/3.9.12-rkxvr6
+# make sure python bin in path, 1 way to do this is to add to your bashrc
+# export PATH="$PATH:$HOME/.local/bin"
 # pip3 install --upgrade fprettify
 
 # run bash ./format.sh in RBC3D directory
