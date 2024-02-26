f7d9125c11b29f31b640ada8b7670166d874c111
suzm10 <suzanmanasreh@gmail.com>
2024-02-26 17:31:24 -0500

    workflow

--- .github/scripts/format.sh
+++ .github/scripts/format.sh
@@ -3,8 +3,8 @@
 echo "formatting example cases and common directory"
 
 echo "$PWD"
-# fprettify ./examples -r --indent 2
-# fprettify ./common -r --indent 2
+fprettify ./examples -r --indent 2
+fprettify ./common -r --indent 2
 
 echo "done"
 
