9f9a6bf524abbd101d401e0afda54981ddbf6875
suzm10 <suzanmanasreh@gmail.com>
2024-06-10 19:02:13 -0400

    rm

--- install/format.sh
+++ install/format.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-echo "formatting example cases and common directory in $PWD"
+echo "Formatting example cases and common directory in $PWD"
 
 pip3 install --upgrade fprettify
 
 fprettify ./examples -r --indent 2
-fprettify ./mycases -r --indent 2
-fprettify ./testcases -r --indent 2
 fprettify ./common -r --indent 2
 
-echo "done"
+echo "Done formatting!"
