442470e1b2b162806962213a8e3835907a2d3ba4
suzm10 <suzanmanasreh@gmail.com>
2024-06-12 23:24:10 -0400

    -j 8

--- install/install-no-petsc.sh
+++ install/install-no-petsc.sh
@@ -9,7 +9,7 @@ cd packages
 # build and install spherepack
 git clone https://github.com/comp-physics/spherepack3.2.git
 cd spherepack3.2
-make
+make -j 8
 
 # build and install makedepf90
 cd ..
@@ -18,7 +18,7 @@ cd ../install/scripts
 echo "PWD $(pwd)"
 python3 mdf90_replace.py
 cd ../../packages/makedepf90
-make
+make -j 8
 make install
 
 echo "Done installing RBC3D!"
