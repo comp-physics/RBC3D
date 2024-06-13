1a711bda8b0205c3a8bc5be37594a4938880ee75
suzm10 <suzanmanasreh@gmail.com>
2024-06-13 12:52:28 -0400

    continue

--- install/install-with-mkl.sh
+++ install/install-with-mkl.sh
@@ -5,13 +5,15 @@
 # echo -e "export PATH="$PATH:$HOME/.local/bin"" >> ~/.bashrc
 # source ~/.bashrc
 
-ml python/3.9.12-rkxvr6 gcc mvapich2 mkl netcdf-c netcdf-cxx netcdf-fortran fftw
+ml anaconda3 gcc/12.3.0 mvapich2/2.3.7-1 intel-oneapi-mkl/2023.1.0 python/3.10.10 netcdf-c/4.9.2-mva2-hdf5-1.14 netcdf-cxx/4.2-mva2-hdf5-1.14 netcdf-fortran/4.6.0-mva2-hdf5-1.14 fftw/3.3.10-mva
 
 mpicc -show
 echo "calling mpicc"
 mpicc
 echo $mpicc
 echo "after calling mpicc"
+
+cat ~/.bashrc
 
 module show mvapich2
 
@@ -40,20 +42,20 @@ cd petsc-3.19.6
 make PETSC_DIR=`pwd` PETSC_ARCH=arch-linux-c-opt all
 make PETSC_DIR=`pwd` PETSC_ARCH=arch-linux-c-opt check
 
-# # build and install spherepack
-# cd ..
-# git clone https://github.com/comp-physics/spherepack3.2.git
-# cd spherepack3.2
-# make
+# build and install spherepack
+cd ..
+git clone https://github.com/comp-physics/spherepack3.2.git
+cd spherepack3.2
+make
 
-# # build and install makedepf90
-# cd ..
-# git clone https://github.com/comp-physics/makedepf90.git
-# cd ../install/scripts
-# echo "PWD $(pwd)"
-# python3 mdf90_replace.py
-# cd ../../packages/makedepf90
-# make
-# make install
+# build and install makedepf90
+cd ..
+git clone https://github.com/comp-physics/makedepf90.git
+cd ../install/scripts
+echo "PWD $(pwd)"
+python3 mdf90_replace.py
+cd ../../packages/makedepf90
+make
+make install
 
-# echo "Done installing RBC3D!"
+echo "Done installing RBC3D!"
