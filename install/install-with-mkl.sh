2fe3b48bc507149161c9825e6af5aebe290d8ae1
suzm10 <suzanmanasreh@gmail.com>
2024-06-14 13:01:16 -0400

    resubmit

--- install/install-with-mkl.sh
+++ install/install-with-mkl.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # salloc a node before you run this because petsc configure uses srun
+source ~/.bashrc
 
 # building and installing petsc 3.19.6 in packages directory
 mkdir packages
