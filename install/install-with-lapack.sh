0eeead3cc7b39433560f0e9b795b88fc8102c95a
suzm10 <suzanmanasreh@gmail.com>
2024-06-13 22:43:21 -0400

    ready?

--- install/install-with-lapack.sh
+++ install/install-with-lapack.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
 # salloc a node before you run this because petsc tests use srun
+set -e -x
 
-# create packages directory
-mkdir packages
-cd packages
+# # create packages directory
+# mkdir packages
+# cd packages
 
-# build and install lapack and blas
-wget https://github.com/Reference-LAPACK/lapack/archive/refs/tags/v3.11.tar.gz
-tar -xf v3.11.tar.gz
-cd lapack-3.11
-cp ../../install/scripts/make.inc ./
-make -j 8
+# # build and install lapack and blas
+# wget https://github.com/Reference-LAPACK/lapack/archive/refs/tags/v3.11.tar.gz
+# tar -xf v3.11.tar.gz
+# cd lapack-3.11
+# cp ../../install/scripts/make.inc ./
+# echo "PWD: `PWD`"
+# make -j 8
 
+cd ..
+# echo "PWD: `PWD`"
 # build and install petsc 3.19.6 in packages directory
 wget https://ftp.mcs.anl.gov/pub/petsc/petsc-3.19.tar.gz
 tar -xf petsc-3.19.tar.gz
+
+parentdir="$(dirname `pwd`)"
+echo "parentdir: $parentdir"
 
 cd petsc-3.19.6
 ./configure --with-cc=mpicc \
@@ -26,9 +33,10 @@ cd petsc-3.19.6
     --COPTFLAGS=-g -O3 -march=native -mtune=native \
     --CXXOPTFLAGS=-g -O3 -march=native -mtune=native \
     --FOPTFLAGS=-g -O3 -march=native -mtune=native \
-    --with-blas-lib= `pwd`/lapack-3.11/liblapack.a \
-    --with-lapack-lib= `pwd`/lapack-3.11/librefblas.a \
+    --with-blas-lib=$parentdir/packages/lapack-3.11/librefblas.a \
+    --with-lapack-lib=$parentdir/packages/lapack-3.11/liblapack.a \
     --with-mpiexec=srun \
+    --with-shared-libraries=0 \
     --with-x11=0 --with-x=0 --with-windows-graphics=0
 
 make PETSC_DIR=`pwd` PETSC_ARCH=petsc_configure all
