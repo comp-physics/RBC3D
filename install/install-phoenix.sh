7c10063db8ba7538899d8935e41c4e6b169ab413
suzm10 <suzanmanasreh@gmail.com>
2024-06-22 12:57:39 -0400

    duplicate

--- install/install-phoenix.sh
+++ install/install-phoenix.sh
@@ -67,11 +67,5 @@ make -j 8
 # build and install makedepf90
 cd ../..
 ./rbc.sh install-makedepf90
-git clone https://github.com/comp-physics/makedepf90.git
-cd ../install/scripts
-python3 mdf90_replace.py
-cd ../../packages/makedepf90
-make -j 8
-make install
 
 echo "Done installing RBC3D!"
