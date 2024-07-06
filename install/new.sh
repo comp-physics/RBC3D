113426acfb2a69a807cda7c82b4af9e7772a19bb
smanasreh6 <smanasreh6@gatech.edu>
2024-07-06 13:23:50 -0400

    ml hdf5

--- install/new.sh
+++ install/new.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm -rf packages2
-mkdir packages2
-cd packages2
+rm -rf packages
+mkdir packages
+cd packages
 
 export ROOTDIR=$(pwd)
 export SRCNCDF=${ROOTDIR}/srcNETCDF
@@ -13,27 +13,57 @@ mkdir $INSNCDF
 
 cd $SRCNCDF
 
-wget downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz
+# wget downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz
 wget downloads.unidata.ucar.edu/netcdf-fortran/4.6.1/netcdf-fortran-4.6.1.tar.gz
 
-tar -xf netcdf-c-4.9.2.tar.gz
+# tar -xf netcdf-c-4.9.2.tar.gz
 tar -xf netcdf-fortran-4.6.1.tar.gz
-git clone https://github.com/Unidata/netcdf-fortran.git
 
-cd netcdf-c-4.9.2
-export CPPFLAGS="-DNDEBUG -DgFortran"
-export CFLAGS="-O"
-export FFLAGS="-O -w"
-export BIN=Linux2_x86_64gfort
+# cd netcdf-c-4.9.2
+# export CPPFLAGS="-DNDEBUG -DgFortran"
+# export CFLAGS="-O"
+# export FFLAGS="-O -w"
+# export BIN=Linux2_x86_64gfort
 
-cd ../netcdf-fortran-4.6.1
-export NCDIR=${INSNCDF}
+./configure --prefix=${INSNCDF} --disable-netcdf-4 --disable-dap
+if (($?)); then
+    echo "[install-ice.sh] Error: NETCDF-C configure failed."
+    exit 1
+fi
+make all check install
+if (($?)); then
+    echo "[install-ice.sh] Error: NETCDF-C tests or install failed."
+    exit 1
+fi
+
+# build and install netcdf-fortran in packages/NETCDF_INST
+cd netcdf-fortran-4.6.1
+# instructions for build with shared libraries
+# https://docs.unidata.ucar.edu/netcdf-c/current/building_netcdf_fortran.html
+export NCDIR=${NETCDF_CROOT}
 export NFDIR=${INSNCDF}
 export CPPFLAGS=$CPPFLAGS" -I${NCDIR}/include"
 export LDFLAGS=$LDFLAGS" -L${NCDIR}/lib"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${NCDIR}/lib
-export LIBS=$LIBS" -lnetcdf"
+
+# copy necessary updated files from this PR until they release new version
+# https://github.com/Unidata/netcdf-fortran/pull/429
+cd fortran
+rm -f module_netcdf_nc_data.F90
+rm -f module_typesizes.F90
+rm -f netcdf.F90
+cp ../../../../install/scripts/module_netcdf_nc_data.F90 ./
+cp ../../../../install/scripts/module_typesizes.F90 ./
+cp ../../../../install/scripts/netcdf.F90 ./
+cd ..
 
 ./configure --prefix=${INSNCDF}
+if (($?)); then
+    echo "[install-ice.sh] Error: NETCDF-Fortran configure failed."
+    exit 1
+fi
 make check
+if (($?)); then
+    echo "[install-ice.sh] Error: NETCDF-Fortran tests failed."
+    exit 1
+fi
 make install
